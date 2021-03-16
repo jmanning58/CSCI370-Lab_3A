9b8dba30cccdf3adc2795b2c040f1194da719ce9
Jacob <manningjc@g.cofc.edu>
2021-03-15 21:13:43 -0400

    Create Student Class and instantiate them

--- app/src/main/java/com/example/csci370_lab_3a/MainActivity.java
+++ app/src/main/java/com/example/csci370_lab_3a/MainActivity.java
@@ -5,27 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.View;
+import java.util.ArrayList;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+import com.example.csci370_lab_3a.Student;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView students;
     private Context mContext;
     private LayoutInflater mInflator;
     //private ArrayList<Student> mDataSource;
 
-    String[] studentFirstName = {"Alli", "George", "Danni", "Alex", "Sarah"};
-    String[] studentLastName = {"Witheren", "Baruso", "Sykes", "Mitchel", "Stapleton"};
-    String[] majors = {"Biology", "Spanish", "History", "Sociology", "Dance"};
+    Student firstStudent = new Student("Alli", "Witheren", "Biology");
+    Student secondStudent = new Student("George", "Baruso", "Spanish");
+    Student thirdStudent = new Student("Danni", "Sykes", "Sociology");
+    Student fourthStudent = new Student("Alex", "Mitchel", "History");
+    Student fifthStudent = new Student("Sarah", "Stapleton", "Dance");
+
+    ArrayList<Student> students = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        students.add(firstStudent);
+        students.add(secondStudent);
+        students.add(thirdStudent);
+        students.add(fourthStudent);
+        students.add(fifthStudent);
+
+        StudentAdapter adapter = new StudentAdapter(getApplicationContext(), students);
     }
 }
---
+++ app/src/main/java/com/example/csci370_lab_3a/Student.java
@@ -0,0 +1,38 @@
+package com.example.csci370_lab_3a;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private String major;
+
+    public Student (String firstName, String lastName, String major) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.major = major;
+    }
+
+    public String getFirstName () {
+            return firstName;
+        }
+
+    public String getLastName () {
+            return lastName;
+        }
+
+    public String getMajor () {
+            return major;
+        }
+
+    public void setFirstName (String firstName) {
+            this.firstName = firstName;
+        }
+
+    public void setLastName (String lastName) {
+            this.lastName = lastName;
+        }
+
+    public void setMajor (String major) {
+            this.major = major;
+        }
+}
